0bdbb923ca23a5cefc0644a2c1dda5ab215638be
Raymond Ho <chunkiat82@gmail.com>
2017-07-01 23:15:38 +0800

    working with express to show the UI

--- examples/express/routes/index.js
+++ examples/express/routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var PrettyProperties = require('pretty-properties');
-const { parseProperties } = require('pretty-properties');
+const {
+    parseProperties
+} = require('pretty-properties');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+    var leftProperties = await parseProperties(__dirname + '/../properties/left.properties');
+    var rightProperties = await parseProperties(__dirname + '/../properties/right.properties');
+    
+    res.render('index', {
+        title: 'Express',
+        leftProperties: JSON.stringify(leftProperties.getProperties()),
+        rightProperties: JSON.stringify(rightProperties.getProperties())
+    });
 });
 
 module.exports = router;
