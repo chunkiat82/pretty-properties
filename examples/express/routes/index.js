aff0274c90fc0667e36ec881ce760b7263b6aa81
Raymond Ho <chunkiat82@gmail.com>
2017-07-01 22:58:08 +0800

    testing usage

--- examples/express/routes/index.js
+++ examples/express/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var PrettyProperties = require('pretty-properties');
+const { parseProperties } = require('pretty-properties');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
