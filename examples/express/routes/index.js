bbde8ae728e7589cfc7d9cea29152d4caeb727f2
Raymond Ho <chunkiat82@gmail.com>
2017-07-10 19:13:22 +0800

    working on properties

--- examples/express/routes/index.js
+++ examples/express/routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
+// <<<<<<< HEAD
 
 
+// =======
+// var PrettyProperties = require('pretty-properties');
+// const {
+//     parseProperties,
+//     diffProperties
+// } = require('pretty-properties');
+
+// let dif2html = require("diff2html").Diff2Html;
+// >>>>>>> working on properties
 /* GET home page. */
 router.get('/', async function(req, res, next) {    
     res.render('index', {
