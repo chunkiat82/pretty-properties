49974a07038871b9c0c8033c447d2a789bc8af2b
Raymond Ho <chunkiat82@gmail.com>
2017-09-22 22:04:31 +0800

    clean state

--- tools/build.js
+++ tools/build.js
@@ -47,6 +47,7 @@ promise = promise.then(() => {
   delete pkg.scripts;
   delete pkg.eslintConfig;
   delete pkg.babel;
+  // pkg.scripts = { "prepare": "npm install" }
   fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
   fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
 });
